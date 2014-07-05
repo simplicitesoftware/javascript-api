92bad64749cee4b7fe0e71447c6ef6978da3c471
dazoulay <dazoulay@simplicite.fr>
2014-07-05 17:54:05 +0200

    Updated

--- simplicite.js
+++ simplicite.js
@@ -2,30 +2,34 @@
  * Simplicit&eacute;&reg; NodeJS lib
  */
 module.exports = {
-  session: function(params) {
-    if (!params) params = {};
-    var debug = params.debug || false;
-    var baseURL = params.baseURL || "http://localhost:8080";
-    var root = params.root || "";
+	session: function(params) {
+		if (!params) params = {};
+		var debug = params.debug || false;
+		var baseURL = params.baseURL || 'http://localhost:8080';
+		var root = params.root || '';
+		var login = params.login || 'public';
+		var password = params.password || '';
 
-    function getBusinessObject(name, instance) {
-      return {
-        metadata: { name: name, instance: instance }
-      }
-    }
+		function getBusinessObject(name, instance) {
+			if (!instance) instance = 'node_' + name;
+			return {
+				metadata: { name: name, instance: instance }
+			}
+		}
 
-    function getBusinessProcess(name) {
-      return {
-        metadata: { name: name }
-      }
-    }
+		function getBusinessProcess(name) {
+			return {
+				metadata: { name: name }
+			}
+		}
 
-    return {
-      debug: debug,
-      baseURL: baseURL,
-      root: root,
-      getBusinessObject: getBusinessObject,
-      getBusinessProcess: getBusinessProcess
-    };
-  }
+		return {
+			metadata: {
+				baseURL: baseURL,
+				root: root
+			},
+			getBusinessObject: getBusinessObject,
+			getBusinessProcess: getBusinessProcess
+		};
+	}
 };
--- test.js
+++ test.js
@@ -1,15 +1,15 @@
-var simplicite = require("simplicite");
+var simplicite = require('./simplicite');
 
 var demo = simplicite.session({
-  url: "http://localhost:8080",
-  root: "demo",
-  login: "designer",
-  password: "designer"
+	url: 'http://localhost:8080',
+	root: 'demo',
+	login: 'designer',
+	password: 'designer'
 });
 console.log(demo.metadata);
 
-var prd = demo.getBusinessObject("DemoProduct");
+var prd = demo.getBusinessObject('DemoProduct');
 console.log(prd.metadata);
 
-var plcord = demo.getBusinessProcess("DemoPlaceNewOrder");
+var plcord = demo.getBusinessProcess('DemoPlaceNewOrder');
 console.log(plcord.metadata);
