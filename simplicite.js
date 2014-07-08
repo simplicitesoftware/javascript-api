2303be870a28c83a19ad0e0144956f9a4296d931
dazoulay <dazoulay@simplicite.fr>
2014-07-08 15:43:51 +0200

    Initiated other methods

--- simplicite.js
+++ simplicite.js
@@ -13,7 +13,8 @@ module.exports = {
 		var login = params.login || 'public';
 		var password = params.password || '';
 
-		var baseURL = params.baseURL || scheme + '://' + host + ':' + port + (root !== '' ? '/' + root : '');
+		if (debug)
+			console.log("[simplicite] Base URL = " + scheme + '://' + host + ':' + port + (root !== '' ? '/' + root : ''));
 
 		var http = require(scheme);
 
@@ -21,34 +22,72 @@ module.exports = {
 			var p = path || '/'; 
 			p = (root !== '' ? '/' + root : '') + p;
 			var m = method || 'GET'; 
-			if (debug) console.log("[call]" + m + ", " + p);
+			if (debug)
+				console.log("[simplicite.call] URL = " + m + ", " + p);
 			http.request({ host: host, port: port, method: m, path: p }, function(res) {
 				var r = ""
 				res.on("data", function (chunk) {
 					r += chunk;
 				});
 				res.on("end", function () {
-					if (callback) callback.call(this, r);
+					if (callback)
+						callback.call(this, r);
 				});
 			}).end();
 		}
 
-		function getBusinessObject(name, instance) {
-			if (!instance) instance = 'node_' + name;
+		function getBusinessObject(name, inst) {
+			if (!inst) inst = 'node_' + name;
+			var path = "/json/obj?object=" + name + "&inst=" + inst;
+
 			var self = this;
+
 			function getMetadata(callback, context) {
 				var self = this;
-				call("/json/obj?object=" + name + "&inst=" + instance + "&action=metadata" + (context ? "&context=" + context : ""), "GET", function(res) {
-					if (debug) console.log("[obj.getMetadata] response = " + res);
+				call(path + "&action=metadata" + (context ? "&context=" + context : ""), "GET", function(res) {
+					if (debug)
+						console.log("[simplicite.BusinessObject.getMetadata] HTTP response = " + res);
 					var r = eval('(' + res + ')');
 					self.metadata = r.response;
-					if (callback) callback.call(self, self.metadata);
+					if (callback)
+						callback.call(self, self.metadata);
 				});
-
 			}
+
+			function search(callback, filters) {
+				// TODO
+			}
+
+			function get(callback, rowId) {
+				// TODO
+			}
+
+			function save(callback, item) {
+				// TODO
+			}
+
+			function create(callback, item) {
+				// TODO
+			}
+
+			function update(callback, item) {
+				// TODO
+			}
+
+			function del(callback, item) {
+				// TODO
+			}
+
 			return {
-				metadata: { name: name, instance: instance },
-				getMetadata: getMetadata
+				metadata: { name: name, instance: inst },
+				getMetadata: getMetadata,
+				search: search,
+				get: get,
+				select: get,
+				save: save,
+				create: create,
+				update: update,
+				del: del
 			}
 		}
 
@@ -64,8 +103,7 @@ module.exports = {
 				host: host,
 				port: port,
 				root: root,
-				login: login,
-				baseURL: baseURL
+				login: login
 			},
 			getBusinessObject: getBusinessObject,
 			getBusinessProcess: getBusinessProcess
