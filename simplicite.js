2380295d39e018836fd19a902765c2f299e3bc77
Simplicite Software <simplicite@simplicite.io>
2018-10-20 11:22:32 +0200

    Sonar code smells fixes

--- simplicite.js
+++ simplicite.js
@@ -173,8 +173,9 @@ module.exports = {
 							error.call(this, err);
 						else
 							throw err;
-					} else if (callback)
+					} else if (callback) {
 						callback.call(this, data, res.statusCode);
+					}
 				});
 		}
 
@@ -202,8 +203,9 @@ module.exports = {
 				var health = parse(res, status);
 				if (health.type === 'error') {
 					(params.error ? params.error : errorHandler).call(self, health.response);
-				} else if (callback)
+				} else if (callback) {
 					callback.call(self, health);
+				}
 			}, function(e) {
 				(params.error ? params.error : errorHandler).call(self, e);
 			});
