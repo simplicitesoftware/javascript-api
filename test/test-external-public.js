177305f9ca31282a74181b16dd2d24fa99f1a420
David AZOULAY <dazoulay@simplicite.fr>
2020-09-15 22:15:54 +0200

    Improved tests

---
+++ test/test-external-public.js
@@ -0,0 +1,28 @@
+const assert = require('assert').strict;
+
+const debug = true;//process.env.TEST_SIMPLICITE_DEBUG == 'true';
+const app = require('../src/simplicite').session({
+	url: process.env.TEST_SIMPLICITE_URL || 'http://localhost:8080',
+	endpoint: 'public',
+	debug: debug
+});
+if (debug) console.log(app.parameters);
+
+const params = { name: 'Simplicite public (URL parameter)' };
+const data = { name: 'Simplicite public (posted in JSON)' };
+
+let ext = app.getExternalObject('AppExt2');
+ext.call(params).then(res => {
+	if (debug) console.log('CALL: ' + JSON.stringify(res, null, 2));
+	assert.ok(res);
+	return ext.call(null, data); // POST call
+}).then(res => {
+	if (debug) console.log('CALL: ' + JSON.stringify(res, null, 2));
+	assert.ok(res);
+	return ext.call(params, data); // POST call w/ params
+}).then(res => {
+	if (debug) console.log('CALL: ' + JSON.stringify(res, null, 2));
+	assert.ok(res);
+}).catch(err => {
+	console.error(err);
+});
--- test/test-external.js
+++ test/test-external.js
@@ -3,7 +3,7 @@ const assert = require('assert').strict;
 const adminUsername = process.env.TEST_SIMPLICITE_ADMIN_USERNAME || 'designer';
 const adminPassword = process.env.TEST_SIMPLICITE_ADMIN_PASSWORD || 'designer';
 
-const debug = process.env.TEST_SIMPLICITE_DEBUG == 'true';
+const debug = true;//process.env.TEST_SIMPLICITE_DEBUG == 'true';
 const app = require('../src/simplicite').session({
 	url: process.env.TEST_SIMPLICITE_URL || 'http://localhost:8080',
 	username: adminUsername, password: adminPassword,
@@ -11,18 +11,26 @@ const app = require('../src/simplicite').session({
 });
 if (debug) console.log(app.parameters);
 
+const params = { name: 'Simplicite (URL parameter)' };
+const data = { name: 'Simplicite (posted in JSON)' };
+
+const extName = 'AppExt2';
 let ext;
 
 app.login().then(res => {
 	if (debug) console.log(res);
 	assert.ok(res.login == adminUsername);
 	console.log('Logged in as ' + res.login);
-	ext = app.getExternalObject('AppExt1');
-	return ext.call({ name: 'Simplicite (URL parameter)' }); // GET call
+	ext = app.getExternalObject(extName);
+	return ext.call(params); // GET call
 }).then(res => {
 	if (debug) console.log('CALL: ' + JSON.stringify(res, null, 2));
 	assert.ok(res);
-	return ext.call(null, { name: 'Simplicite (posted in JSON)' }); // POST call
+	return ext.call(null, data); // POST call
+}).then(res => {
+	if (debug) console.log('CALL: ' + JSON.stringify(res, null, 2));
+	assert.ok(res);
+	return ext.call(params, data); // POST call w/ params
 }).then(res => {
 	if (debug) console.log('CALL: ' + JSON.stringify(res, null, 2));
 	assert.ok(res);
