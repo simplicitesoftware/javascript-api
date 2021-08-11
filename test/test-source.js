b2e81b6a8b92b31681eae9580c0c1afdd7c212e9
David AZOULAY <dazoulay@simplicite.fr>
2021-08-11 16:38:08 +0200

    Source update test

---
+++ test/test-source.js
@@ -0,0 +1,38 @@
+const adminUsername = process.env.TEST_SIMPLICITE_ADMIN_USERNAME || 'designer';
+const adminPassword = process.env.TEST_SIMPLICITE_ADMIN_PASSWORD || 'designer';
+
+const debug = process.env.TEST_SIMPLICITE_DEBUG == 'true';
+const app = require('../src/simplicite').session({
+	url: process.env.TEST_SIMPLICITE_URL || 'http://localhost:8080',
+	username: adminUsername, password: adminPassword,
+	debug: debug
+});
+if (debug) console.log(app.parameters);
+
+let obj;
+
+app.login().then(res => {
+	if (debug) console.log(res);
+	console.log('Logged in as ' + res.login);
+	obj = app.getBusinessObject('ObjectInternal');
+	return obj.search({ obo_name: 'AppTestSrc' });
+}).then(list => {
+	if (debug) console.log(list);
+	return obj.getForUpdate(list[0].row_id, { inlineDocuments: true });
+}).then(item => {
+	if (debug) console.log(item);
+	let doc = obj.getFieldDocument("obo_script_id");
+	if (debug) console.log(doc);
+	let src = doc.getContentAsText();
+	if (debug) console.log(src);
+	doc.setContentFromText(src += '\n// ' + new Date()); 
+	item.obo_script_id = doc.getValue();
+	return obj.update(item, { inlineDocuments: true });
+}).then(item => {
+	if (debug) console.log(item);
+	return app.logout();
+}).then(() => {
+	console.log('Logged out');
+}).catch(err => {
+	console.error(err);
+});
